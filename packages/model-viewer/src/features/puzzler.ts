4dca00836b7bdfda3de4321025d2f1c45b871a93
Adrian von Gegerfelt <adrian@designbyadrian.com>
2022-11-24 14:19:39 +0100

    ⚡️ Puzzler position and rotation

---
+++ packages/model-viewer/src/features/puzzler.ts
@@ -0,0 +1,68 @@
+import { Object3D } from 'three';
+import ModelViewerElementBase, {
+  $scene,
+  $onModelLoad,
+} from '../model-viewer-base.js';
+import { Constructor, debounce } from '../utilities.js';
+
+export declare interface PuzzlerInterface {
+  updateNodePosition(name: string, position: [number, number, number]): void;
+  updateNodeRotation(name: string, rotation: [number, number, number]): void;
+}
+
+const $meshes = Symbol('meshes');
+
+export const PuzzlerMixin = <T extends Constructor<ModelViewerElementBase>>(
+  ModelViewerElement: T
+): Constructor<PuzzlerInterface> & T => {
+  class PuzzlerModelViewerElement extends ModelViewerElement {
+    private [$meshes] = new Map<string, Object3D>();
+
+    private updateNodePositionDebounced = debounce(async () => {
+      this[$scene].updateBoundingBox();
+      this[$scene].updateShadow();
+      await this[$scene].updateFraming();
+      this[$scene].queueRender();
+    }, 300);
+
+    updateNodePosition(name: string, position: [number, number, number]) {
+      const node = this[$meshes].get(name);
+      if (node) {
+        node.position.set(...position);
+        this[$scene].updateShadow();
+        this[$scene].queueRender();
+        this.updateNodePositionDebounced();
+      }
+    }
+
+    updateNodeRotation(name: string, rotation: [number, number, number]) {
+      const node = this[$meshes].get(name);
+      if (node) {
+        node.rotation.set(...rotation);
+        this[$scene].updateShadow();
+        this[$scene].queueRender();
+        this.updateNodePositionDebounced();
+      }
+    }
+
+    [$onModelLoad]() {
+      super[$onModelLoad]();
+
+      const { currentGLTF } = this[$scene];
+
+      if (currentGLTF != null) {
+        const scene = this[$scene];
+
+        this[$meshes].clear();
+
+        scene.traverse((node) => {
+          if (node.type === 'Mesh' && node.name.length) {
+            this[$meshes].set(node.name, node);
+          }
+        });
+      }
+    }
+  }
+
+  return PuzzlerModelViewerElement;
+};
