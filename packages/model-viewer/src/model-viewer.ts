2b771499127ae9766b894689604d8059bdcc4471
Adrian von Gegerfelt <adrian@designbyadrian.com>
2022-11-24 14:19:39 +0100

    ⚡️ Puzzler position and rotation

--- packages/model-viewer/src/model-viewer.ts
+++ packages/model-viewer/src/model-viewer.ts
@@ -13,28 +13,45 @@
  * limitations under the License.
  */
 
-import {AnimationMixin} from './features/animation.js';
-import {AnnotationMixin} from './features/annotation.js';
-import {ARMixin} from './features/ar.js';
-import {ControlsMixin} from './features/controls.js';
-import {EnvironmentMixin} from './features/environment.js';
-import {LoadingMixin} from './features/loading.js';
-import {SceneGraphMixin} from './features/scene-graph.js';
-import {StagingMixin} from './features/staging.js';
+import { AnimationMixin } from './features/animation.js';
+import { AnnotationMixin } from './features/annotation.js';
+import { ARMixin } from './features/ar.js';
+import { ControlsMixin } from './features/controls.js';
+import { EnvironmentMixin } from './features/environment.js';
+import { LoadingMixin } from './features/loading.js';
+import { PuzzlerMixin } from './features/puzzler.js';
+import { SceneGraphMixin } from './features/scene-graph.js';
+import { StagingMixin } from './features/staging.js';
 import ModelViewerElementBase from './model-viewer-base.js';
-import {FocusVisiblePolyfillMixin} from './utilities/focus-visible.js';
+import { FocusVisiblePolyfillMixin } from './utilities/focus-visible.js';
 
 // Uncomment these lines to export PMREM textures in Glitch:
 // export {default as TextureUtils} from './three-components/TextureUtils';
 // export * from 'three';
 
-export const ModelViewerElement = AnnotationMixin(SceneGraphMixin(StagingMixin(
-    EnvironmentMixin(ControlsMixin(ARMixin(LoadingMixin(AnimationMixin(
-        FocusVisiblePolyfillMixin(ModelViewerElementBase)))))))));
+export const ModelViewerElement = AnnotationMixin(
+  SceneGraphMixin(
+    PuzzlerMixin(
+      StagingMixin(
+        EnvironmentMixin(
+          ControlsMixin(
+            ARMixin(
+              LoadingMixin(
+                AnimationMixin(
+                  FocusVisiblePolyfillMixin(ModelViewerElementBase)
+                )
+              )
+            )
+          )
+        )
+      )
+    )
+  )
+);
 
 export type ModelViewerElement = InstanceType<typeof ModelViewerElement>;
 
-export {RGB, RGBA} from './three-components/gltf-instance/gltf-2.0';
+export { RGB, RGBA } from './three-components/gltf-instance/gltf-2.0';
 
 customElements.define('model-viewer', ModelViewerElement);
 
