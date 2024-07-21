8520ef00a96a87f8ed8790183a739dd2e37a0d4f
Adrian von Gegerfelt <adrian@designbyadrian.com>
2024-07-21 18:58:05 +0200

    Squashed commit of the following:
    commit ae642adf887c5bd8db7261451315baa944dba22d
    Author: Adrian von Gegerfelt <adrian@designbyadrian.com>
    Date:   Sun Jul 21 13:13:44 2024 +0200
    
        ✨ Just add water
    
    commit e131f8632c7da80804dffb246bf65adc52942609
    Author: Adrian von Gegerfelt <adrian@designbyadrian.com>
    Date:   Sat Jul 20 19:06:23 2024 +0200
    
        🎉 static water loading

---
+++ packages/model-viewer/src/features/ld-environment.ts
@@ -0,0 +1,84 @@
+import { property } from 'lit/decorators.js';
+
+import ModelViewerElementBase, {
+  $scene,
+  $needsRender,
+} from '../model-viewer-base.js';
+
+import { Constructor } from '../utilities.js';
+
+import {Water} from './ld-environment/water.js';
+import { PlaneGeometry, RepeatWrapping, TextureLoader, Vector3 } from 'three';
+
+const $justAddWater = Symbol('justAddWater');
+const $animateEnvironment = Symbol('animateEnvironment');
+const $water = Symbol('water');
+
+export declare interface LDEnvironmentInterface {
+  waterTexture: string|null;
+}
+
+export const LDEnvironmentMixin = <
+  T extends Constructor<ModelViewerElementBase>
+>(
+  ModelViewerElement: T
+): Constructor<LDEnvironmentInterface> & T => {
+  class LDEnvironmentModelViewerElement extends ModelViewerElement {
+    @property({type: String, attribute: 'water-texture'})
+    waterTexture: string|null = null;
+
+    private [$water]: Water|null = null;
+
+
+
+    private[$animateEnvironment]() {
+      if(this[$water]) {
+        this[$water].animate();
+        this[$needsRender]();
+        requestAnimationFrame(()=>{this[$animateEnvironment]()});
+      }
+    }
+
+    private[$justAddWater]() {
+      if (this[$scene]) {
+        const {waterTexture} = this;
+
+        const waterGeometry = new PlaneGeometry( 10000, 10000 );
+
+        this[$water] = new Water(
+					waterGeometry,
+					{
+						textureWidth: 512,
+						textureHeight: 512,
+						waterNormals: new TextureLoader().load( waterTexture!, function ( texture ) {
+							texture.wrapS = texture.wrapT = RepeatWrapping;
+						} ),
+						sunDirection: new Vector3(),
+						sunColor: 0xffffff,
+						waterColor: 0x001e0f,
+						distortionScale: 3.7,
+						fog: this[$scene].fog !== undefined
+					}
+				);
+
+        /* This to make sure plane is at floor level */
+				this[$water].rotation.x = - Math.PI / 2;
+
+				this[$scene].add( this[$water] );
+
+        this[$animateEnvironment]();
+      }
+    }
+
+    updated(changedProperties: Map<string|number|symbol, unknown>) {
+      super.updated(changedProperties);
+
+      if (changedProperties.has('waterTexture')) {
+        this[$justAddWater]();
+        this[$needsRender]();
+      }
+    }
+  }
+
+  return LDEnvironmentModelViewerElement;
+};
