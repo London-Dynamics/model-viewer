44c2d63b5337b5e0117d4aac2817857ed78a6218
Adrian von Gegerfelt <adrian@designbyadrian.com>
2024-11-28 13:46:51 +0100

    ✨ added deDraco to window

--- packages/model-viewer/src/features/ld-puzzler.ts
+++ packages/model-viewer/src/features/ld-puzzler.ts
@@ -1,7 +1,18 @@
+declare global {
+  interface Window {
+    deDraco: any;
+  }
+}
+
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
+import { GLTFExporter } from 'three/examples/jsm/exporters/GLTFExporter.js';
+
 import ModelViewerElementBase from '../model-viewer-base.js';
 
 import { Constructor } from '../utilities.js';
 import {createSafeObjectUrlFromArrayBuffer} from '../utilities/create_object_url.js';
+
 
 export declare interface LDPuzzlerInterface {
   setSrcFromBuffer(buffer: ArrayBuffer): void;
@@ -22,7 +33,55 @@ export const LDPuzzlerMixin = <
         console.error(e);
       }
     }
+
+    /* Remove draco compression from a glb
+    *
+    * @param {ArrayBuffer} inputBuffer GLB with draco
+    * @return {Promise<ArrayBuffer>} GLB without draco
+    */
+    deDraco(inputBuffer:ArrayBuffer) {
+      return new Promise((res) => {
+        const loader = new GLTFLoader()
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath(
+          'https://cdn.jsdelivr.net/npm/three@0.170.0/examples/jsm/libs/draco/',
+        )
+        loader.setDRACOLoader(dracoLoader)
+
+        loader.parse(
+          inputBuffer,
+          '',
+          (model) => {
+            if (model.scene) {
+              const exporter = new GLTFExporter()
+              exporter.parse(
+                model.scene,
+                (arrayBuffer) => {
+                  res(arrayBuffer)
+                },
+                function (err) {
+                  console.error(err)
+                },
+                { binary: true },
+              )
+            }
+          },
+          (error) => {
+            console.error(error)
+          },
+        )
+      })
+    }
+
+    connectedCallback() {
+      super.connectedCallback();
+
+      if (typeof window !== 'undefined') {
+        window.deDraco = this.deDraco;
+      }
+    }
   }
+
 
   return LDPuzzlerModelViewerElement;
 };
